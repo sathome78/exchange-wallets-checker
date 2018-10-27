fc6151fef0e4662e0e05263c3a34350448bc4123
DmitrenkoNIkitaa <nikita.dmytrenko@upholding.biz>
2018-10-27 18:28:02 +0300

    Optimize templates.

--- src/main/java/com/example/demo/schedulers/NotificatorService.java
+++ src/main/java/com/example/demo/schedulers/NotificatorService.java
@@ -13,22 +13,22 @@ public interface NotificatorService {
     DateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss");
 
     String ABOVE_MAX_LIMIT =
-            "\uD83C\uDF4E Кошелёк: %s  Превышен максимальный лимит!  Время: %s\n\n " +
-            "Баланс: %s   Баланс в $ : %s\n\n" +
-            "Мин.лимит: %s   Max.Лимит: %s\n"+
-            "Мин.лимит в $: %s   Max.Лимит в $: %s\n";
+            "\uD83C\uDF4E Кошелёк: %s\nПревышен максимальный лимит!\nВремя: %s\n\n" +
+            "Баланс: %s  \nБаланс в $: %s\n\n" +
+            "Мин.лимит: %s \nMax.Лимит: %s\n\n"+
+            "Мин.лимит в $: %s \nMax.Лимит в $: %s\n";
 
     String LOW_THAN_MIN_AMOUNT  =
-            "\uD83C\uDF4B Кошелёк: %s  Ниже нижнего лимита!    Время: %s\n\n " +
-            "Баланс: %s   Баланс в $: %s\n\n" +
-            "Мин.лимит: %s   Max.Лимит: %s\n"+
-            "Мин.лимит в $: %s   Max.Лимит в $: %s\n";
+            "\uD83C\uDF4B Кошелёк: %s\nНиже нижнего лимита!\nВремя: %s\n\n" +
+            "Баланс: %s  \nБаланс в $: %s\n\n" +
+            "Мин.лимит: %s \nMax.Лимит: %s\n\n"+
+            "Мин.лимит в $: %s \nMax.Лимит в $: %s\n";
 
     String PERMISSIBLE_RANGE =
-            "\uD83C\uDF4F Кошелёк: %s  Количество в допустимом диапазоне!    Время: %s\n\n " +
-            "Баланс: %s   Баланс в $: %s\n\n" +
-            "Мин.лимит: %s   Max.Лимит: %s\n"+
-            "Мин.лимит в $: %s   Max.Лимит в $: %s\n";
+            "\uD83C\uDF4F Кошелёк: %s\nКоличество в допустимом диапазоне!\nВремя: %s\n\n" +
+            "Баланс: %s  \nБаланс в $: %s\n\n" +
+            "Мин.лимит: %s \nMax.Лимит: %s\n\n"+
+            "Мин.лимит в $: %s \nMax.Лимит в $: %s\n";
 
 
     void notificate(String template, Coin coin);
