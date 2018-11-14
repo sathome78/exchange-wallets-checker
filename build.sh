bef98b4e18a064f9aab8b82ac86144f52184f193
DmitrenkoNIkitaa <nikita.dmytrenko@upholding.biz>
2018-11-14 11:23:12 +0200

    Add command for moving jar.

--- build.sh
+++ build.sh
@@ -3,6 +3,8 @@
 
 docker build -t wallet-checker-app .
 
+mv /Users/Nikita/Documents/exrates-wallet-checker/target/wallet-checker-app.jar /Users/Nikita/Documents
+
 ssh -i ndmitrenko.pem ndmitrenko@172.31.3.72
 
 scp -i ndmitrenko.pem wallet-checker-app.jar ndmitrenko@172.31.3.72:~/
